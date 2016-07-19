082c761791f97129b9f0e731f82b0f6783aed787
Ozan Can Altiok <ozymaxx@gmail.com>
2016-07-19 14:46:16 +0300

    init commit

---
+++ app/src/main/java/summerresearch/iui/ku/autocompletiondemo/MainActivity.java
@@ -0,0 +1,16 @@
+package summerresearch.iui.ku.autocompletiondemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// the main activity of the application
+// it'll include the drawing canvas
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
