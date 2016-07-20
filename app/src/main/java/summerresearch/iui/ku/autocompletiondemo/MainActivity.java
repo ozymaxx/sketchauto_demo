598c3f812d93125416d06cf628e50c6be73ec857
ElifYagmur <elifygmr@hotmail.com>
2016-07-20 11:23:55 +0300

    v.1.2

--- app/src/main/java/summerresearch/iui/ku/autocompletiondemo/MainActivity.java
+++ app/src/main/java/summerresearch/iui/ku/autocompletiondemo/MainActivity.java
@@ -1,5 +1,6 @@
 package summerresearch.iui.ku.autocompletiondemo;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         setContentView(R.layout.activity_main);
     }
 }
