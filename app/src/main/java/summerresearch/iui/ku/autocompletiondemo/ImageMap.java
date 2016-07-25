7ca8843d844a74e98a351759ceaa98aec171d5c5
ElifYagmur <elifygmr@hotmail.com>
2016-07-25 16:25:48 +0300

    with everything

--- app/src/main/java/summerresearch/iui/ku/autocompletiondemo/ImageMap.java
+++ app/src/main/java/summerresearch/iui/ku/autocompletiondemo/ImageMap.java
@@ -16,6 +16,115 @@ public class ImageMap {
             put("angel", R.mipmap.angel );
             put("ant", R.mipmap.ant );
             put("apple", R.mipmap.apple );
+
+            put("arm", R.mipmap.arm );
+            put("armchair", R.mipmap.armchair );
+            put("ashtray", R.mipmap.ashtray );
+            put("axe", R.mipmap.axe );
+            put("backpack", R.mipmap.backpack );
+
+            put("banana", R.mipmap.banana );
+            put("barn", R.mipmap.barn );
+            put("baseball bat", R.mipmap.baseballbat );
+            put("basket", R.mipmap.basket );
+            put("bathtub", R.mipmap.bathtub );
+
+            put("bear(animal)", R.mipmap.bear );
+            put("bed", R.mipmap.bed );
+            put("bee", R.mipmap.bee );
+            put("beer-mug", R.mipmap.beermug );
+            put("bell", R.mipmap.bell);
+
+            put("bench", R.mipmap.bench );
+            put("bicycle", R.mipmap.bicycle );
+            put("binoculars", R.mipmap.binoculars );
+            put("blimp", R.mipmap.blimp );
+            put("book", R.mipmap.book );
+
+            put("bookshelf", R.mipmap.bookshelf );
+            put("boomerang", R.mipmap.boomerang );
+            put("bottle opener", R.mipmap.bottleopener );
+            put("bowl", R.mipmap.bowl);
+            put("brain", R.mipmap.brain );
+
+            put("bread", R.mipmap.bread );
+            put("bridge", R.mipmap.bridge );
+            put("bulldozer", R.mipmap.bulldozer );
+            put("bus", R.mipmap.bus );
+            put("bush", R.mipmap.bush );
+
+            put("butterfly", R.mipmap.butterfly );
+            put("cabinet", R.mipmap.cabinet );
+            put("cactus", R.mipmap.cactus );
+            put("cake", R.mipmap.cake );
+            put("calculator", R.mipmap.calculator );
+
+            put("camel", R.mipmap.camel );
+            put("camera", R.mipmap.camera );
+            put("candle", R.mipmap.candle );
+            put("cannon", R.mipmap.cannon );
+            put("canoe", R.mipmap.canoe );
+
+            put("car (sedan)", R.mipmap.car );
+            put("carrot", R.mipmap.carrot );
+            put("castle", R.mipmap.castle );
+            put("cat", R.mipmap.cat );
+            put("cell phone", R.mipmap.cellphone );
+
+            put("chair", R.mipmap.chair );
+            put("chandelier", R.mipmap.chandelier );
+            put("church", R.mipmap.church );
+            put("cigarette", R.mipmap.cigarette );
+            put("cloud", R.mipmap.cloud );
+
+            put("comb", R.mipmap.comb );
+            put("computer monitor", R.mipmap.computermonitor );
+            put("computer-mouse", R.mipmap.computermouse );
+            put("couch", R.mipmap.couch );
+            put("cow", R.mipmap.cow );
+
+            put("crab", R.mipmap.crab );
+            put("crane (machine)", R.mipmap.crane );
+            put("crocodile", R.mipmap.crocodile );
+            put("crown", R.mipmap.crown );
+            put("cup", R.mipmap.cup );
+
+            put("diamond", R.mipmap.diamond );
+            put("dog", R.mipmap.dog );
+            put("dolphin", R.mipmap.dolphin );
+            put("donut", R.mipmap.donut );
+            put("door", R.mipmap.door );
+
+            put("door handle", R.mipmap.doorhandle );
+            put("dragon", R.mipmap.dragon );
+            put("duck", R.mipmap.duck );
+            put("ear", R.mipmap.ear );
+            put("elephant", R.mipmap.elephant );
+
+            put("envelope", R.mipmap.envelope );
+            put("eye", R.mipmap.eye );
+            put("eyeglasses", R.mipmap.eyeglasses );
+            put("face", R.mipmap.face );
+            put("fan", R.mipmap.fan );
+
+            put("feather", R.mipmap.feather );
+            put("fire hydrant", R.mipmap.firehydrant );
+            put("fish", R.mipmap.fish );
+            put("flashlight", R.mipmap.flashlight );
+            put("floor lamp", R.mipmap.floorlamp );
+
+            put("flower with stem", R.mipmap.flowerwithstem );
+            put("flying bird", R.mipmap.flyingbird );
+            put("flying saucer", R.mipmap.flyingsaucer );
+            put("foot", R.mipmap.foot );
+            put("fork", R.mipmap.fork );
+
+            put("forg", R.mipmap.forg );
+            put("frying-pan", R.mipmap.fryingpan );
+            put("giraffe", R.mipmap.giraffe );
+            put("grapes", R.mipmap.grapes );
+            put("grenade", R.mipmap.grenade );
+
         }};
     }
 
